cac109f1e002c2e55dbd78cdd375e461cdaef4b8
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-11-02 16:22:50 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// Create a movie list.
+let movies = [
+    {
+        name: 'Mad Max: Fury Road',
+        released: '2015'
+    },
+    {
+        name: 'The pianist',
+        released: '2002'
+    },
+    {
+        name: 'The Big Short',
+        released: '2017'
+    },
+    {
+        name: 'No Country For Old Man',
+        released: '2007'
+    },
+    {
+        name: 'Master & Commander',
+        released: '2003'
+    },
+    {
+        name: 'Kill Bill',
+        released: '2003'
+    },
+    {
+        name: 'Kill Bill II',
+        released: '2004'
+    },
+    {
+        name: 'Birdman',
+        released: '2014'
+    }
+]
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix app!');
+});
+app.get('/movies', (req, res) => {
+    res.send(movies)
+});
+app.get('/movies', (req, res) => {
+    res.send(movies)
+});
+
+app.get('/documentation.html', (req, res) => {
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('There is something wrong..really wrong...')
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
