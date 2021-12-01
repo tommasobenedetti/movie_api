5169b12ed4385ec50c27e4f96ec39859d813a2b7
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-12-01 11:22:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const express = require('express');
