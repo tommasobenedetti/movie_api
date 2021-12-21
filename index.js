7b8f93e3bbd25da4bb15778c895c37030b38ca53
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-12-21 11:57:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix.herokuapp.com/movies'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://quiet-savannah-08380.herokuapp.com/'];
 
 app.use(cors({
   origin: (origin, callback) => {
