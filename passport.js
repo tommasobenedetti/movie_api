5a214e6a47c961ec1fb1be5b6fe89695aafe4f6c
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-12-31 10:27:14 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -30,7 +30,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'Lello2019'
+  secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
